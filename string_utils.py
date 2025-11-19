75da9d10011d1ab8449c0b6db17ccc96166ef27d
tomerer1415-web <tomerer1415@gmail.com>
2025-11-19 12:53:29 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -6,7 +6,6 @@ def split_at_first_digit(formula):
             break
         digit_location += 1
 
-    # This check MUST be outside the loop
     if digit_location == len(formula):
         return formula, 1
 
@@ -16,20 +15,15 @@ def split_at_first_digit(formula):
 
 
 def split_before_each_uppercase(formula):
-    digit_location = 1
+    result = []
+    current = ""
 
-    for char in formula[1:]:
-        if char.isdigit():
-            break
-        digit_location += 1
+    for char in formula:
+        if char.isupper() and current:
+            result.append(current)
+            current = char
+        else:
+            current += char
 
-    if digit_location == len(formula):
-        return formula, 1
-
-    prefix = formula[:digit_location]
-    number = int(formula[digit_location:])
-    return prefix, number
-
-# Test your code with your input
-formula = "Ir6"
-print(split_at_first_digit(formula))
+    result.append(current)
+    return result
