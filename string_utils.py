ee738dd12af6ee27874ad2cdfa076d5160e124e4
tomerer1415-web <tomerer1415@gmail.com>
2025-11-19 12:40:54 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,19 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+
+    if digit_location == len(formula):
+        return formula, 1
+
+    prefix = formula[:digit_location]
+    number = int(formula[digit_location:])
+    return prefix, number
+
+formula = "Ir6"
+print(split_at_first_digit(formula))
